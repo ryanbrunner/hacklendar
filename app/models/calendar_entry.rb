43f1ad409fb621f7d05a8a0b8429ea968ababf40
Ryan Brunner <ryan@ryanbrunner.com>
2012-10-02 20:44:51 -0400

    Adding a scope and some validation methods

--- app/models/calendar_entry.rb
+++ app/models/calendar_entry.rb
@@ -1,3 +1,14 @@
 class CalendarEntry < ActiveRecord::Base
   attr_accessible :entry, :entry_at, :title
+
+  validates :title, :presence => true
+  validates :entry, :presence => true
+  validates :entry_at, :presence => true
+
+  def self.for_month(year, month)
+    start_date = Date.new(year, month).beginning_of_month
+    end_date = Date.new(year, month).end_of_month
+
+    where(:entry_at => start_date..end_date)
+  end
 end
